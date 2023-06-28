3cdca18c4fea97c89472f5f2dd0f2fb5108771b3
Génesis Mauries <exodo_54@hotmail.com>
2023-06-28 11:09:09 -0600

    all functions

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,60 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return text.trim() === "" ? 0 : text.split(" ").length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.trim() === "" ? 0 : text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let sum = 0;
+    for (let i = 0; i < text.length; i += 1) {
+      const textCode = text.charCodeAt(i);
+      if (
+        (textCode >= 65 && textCode <= 90) ||
+        (textCode >= 97 && textCode <= 122) ||
+        (textCode >= 48 && textCode <= 57)
+      ) {
+        sum++;
+      }
+    }
+    return sum;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+    let sum = 0;
+
+    for (let i = 0; i < words.length; i++) {
+      sum += words[i].length;
+    }
+    return (sum / words.length).toFixed(2);
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+    let numberCount = 0;
+    for (let i = 0; i < words.length; i++) {
+      if (Number(words[i])) {
+        numberCount++;
+      }
+    }
+    return numberCount;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+    let numberSum = 0;
+    for (let i = 0; i < words.length; i++) {
+      if (Number(words[i])) {
+        // console.log(char)
+        numberSum += parseFloat(words[i]);
+      }
+    }
+
+    return numberSum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const userInput = document.querySelector('textarea[name = "user-input"]');
+function textAnalyzer(){
+  document.querySelectorAll('li')[0].innerHTML =`Palabras: ${analyzer.getWordCount(userInput.value)}`;
+  document.querySelectorAll('li')[1].innerHTML =`Cantidad: ${analyzer.getCharacterCount(userInput.value)}`;
+  document.querySelectorAll('li')[2].innerHTML =`Caracteres sin espacio: ${analyzer.getCharacterCountExcludingSpaces(userInput.value)}`;
+  document.querySelectorAll('li')[3].innerHTML =`Promedio longitud: ${analyzer.getAverageWordLength(userInput.value)}`;
+  document.querySelectorAll('li')[4].innerHTML =`Números: ${analyzer.getNumberCount(userInput.value)}`;
+  document.querySelectorAll('li')[5].innerHTML =`Suma números: ${analyzer.getNumberSum(userInput.value)}`;
+}
+userInput.addEventListener('keyup', textAnalyzer);
+
+const resetButton = document.querySelector('button');
+function reset(){
+  userInput.value = '';
+  document.querySelectorAll('li')[0].innerHTML =`Palabras: 0`;
+  document.querySelectorAll('li')[1].innerHTML =`Cantidad: 0`;
+  document.querySelectorAll('li')[2].innerHTML =`Caracteres sin espacio: 0`;
+  document.querySelectorAll('li')[3].innerHTML =`Promedio longitud: 0`;
+  document.querySelectorAll('li')[4].innerHTML =`Números: 0`;
+  document.querySelectorAll('li')[5].innerHTML =`Suma números: 0`;
+}
+resetButton.addEventListener('click', reset);
